99d6c42c99f0667f4f661944493fef1d53a57641
201 <201@academicos.cesde.edu.co>
2022-08-02 12:08:14 -0500

    primera parte

---
+++ src/controllers/controladorimg.js
@@ -0,0 +1,134 @@
+
+let imagen=document.getElementById('img')
+
+  imagen.addEventListener("mouseover",function(evento){
+        
+        let titulo=document.getElementById('titulo')
+        titulo.classList.remove('invisible')
+    
+        let texto=document.getElementById('texto')
+        texto.classList.remove('invisible')
+        let precio1=document.getElementById("precio1")
+        precio1.classList.remove('invisible')
+
+        let precio2=document.getElementById("precio2")
+        precio2.classList.remove('invisible')})   
+
+
+        
+        imagen.addEventListener("mouseleave",function(evento){
+        
+            let titulo=document.getElementById('titulo')
+            titulo.classList.add('invisible')
+        
+            let texto=document.getElementById('texto')
+            texto.classList.add('invisible')
+
+            let precio1=document.getElementById("precio1")
+            precio1.classList.add('invisible')
+    
+            let precio2=document.getElementById("precio2")
+            precio2.classList.add('invisible')
+        })  
+
+           
+            
+            
+            
+
+let imagen1=document.getElementById('img1')
+
+  imagen1.addEventListener("mouseover",function(evento){
+    
+    let titulo1=document.getElementById('titulo1')
+        titulo1.classList.remove('invisible')
+        let texto1=document.getElementById('texto1')
+        texto1.classList.remove('invisible')
+         
+        let precio3=document.getElementById("precio3")
+        precio3.classList.remove('invisible')
+        let precio4=document.getElementById("precio4")
+        precio4.classList.remove('invisible')
+    
+    })
+        imagen1.addEventListener("mouseleave",function(evento){
+    
+            let titulo1=document.getElementById('titulo1')
+                titulo1.classList.add('invisible')
+                let texto1=document.getElementById('texto1')
+                texto1.classList.add('invisible')
+            
+                let precio3=document.getElementById("precio3")
+                precio3.classList.add('invisible')
+        
+                let precio4=document.getElementById("precio4")
+                precio4.classList.add('invisible')})
+     
+        
+
+
+let imagen2=document.getElementById('img2')
+
+  imagen2.addEventListener("mouseover",function(evento){
+    
+    let titulo2=document.getElementById('titulo2')
+        titulo2.classList.remove('invisible')
+        let texto2=document.getElementById('texto2')
+        texto2.classList.remove('invisible')
+        let precio5=document.getElementById("precio5")
+        precio5.classList.remove('invisible')
+        let precio6=document.getElementById("precio6")
+        precio6.classList.remove('invisible')})
+
+
+
+
+        imagen2.addEventListener("mouseleave",function(evento){
+    
+            let titulo2=document.getElementById('titulo2')
+                titulo2.classList.add('invisible')
+                let texto2=document.getElementById('texto2')
+                texto2.classList.add('invisible')
+                let precio5=document.getElementById("precio5")
+                precio5.classList.add('invisible')
+        
+                let precio6=document.getElementById("precio6")
+                precio6.classList.add('invisible')            
+            })
+
+
+
+
+let imagen3=document.getElementById('img3')
+
+  imagen3.addEventListener("mouseover",function(evento){
+    
+
+    let titulo3=document.getElementById('titulo3')
+        titulo3.classList.remove('invisible')
+        let texto3=document.getElementById('texto3')
+        texto3.classList.remove('invisible')
+    
+        let precio7=document.getElementById("precio7")
+        precio7.classList.remove('invisible')
+        let precio8=document.getElementById("precio8")
+        precio8.classList.remove('invisible')
+    })
+
+        imagen3.addEventListener("mouseleave",function(evento){
+    
+
+            let titulo3=document.getElementById('titulo3')
+                titulo3.classList.add('invisible')
+                let texto3=document.getElementById('texto3')
+                texto3.classList.add('invisible')
+            
+                let precio7=document.getElementById("precio7")
+                precio7.classList.add('invisible')
+        
+                let precio8=document.getElementById("precio8")
+                precio8.classList.add('invisible')})
+        
+
+
+
